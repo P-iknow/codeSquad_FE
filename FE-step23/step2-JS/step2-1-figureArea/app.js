ab23e3e60ffb21a5c2fb8c35c00bb799ad49445a
sel-dev <apricotsoul@gmail.com>
2019-04-12 15:20:16 +0900

    Fix - getArea 함수를 모듈

--- FE-step23/step2-JS/step2-1-figureArea/app.js
+++ FE-step23/step2-JS/step2-1-figureArea/app.js
@@ -1,3 +1,16 @@
-const getArea = require("./figureArea")
+const calc = require("./figureArea")
 
-getArea.circle(5)
+
+function testcircle(){
+    let answer = 0
+    for(let i=1;i<6;i++){
+        answer += calc.circle([i])
+    }
+    return answer
+}
+console.log(calc.getArea("circle", 3))   
+console.log(calc.getArea("circle", 1, 5))
+console.log(calc.getArea("rect", 2, 4)) 
+console.log(calc.getArea("trepzoid", 3, 3, 3)) 
+console.log(calc.getArea("cylinder", 2, 4))
+calc.printExecutionSequence()
--- FE-step23/step2-JS/step2-1-figureArea/figureArea.js
+++ FE-step23/step2-JS/step2-1-figureArea/figureArea.js
@@ -20,33 +20,59 @@ function checkParamCount(count, param) {
 function checkParam(count, param){
     return isNum(param) && checkParamCount(count, param) && param;
 }
-function circle(...param) {
+
+function circle(param) {
     const [r] = checkParam(1, param);
     return Math.PI * r * r 
 }
 
-function rect(...param) {
+function rect(param) {
     const [w, h] = checkParam(2,param); 
     return w * h;
 }
 
-function trepzoid(...param) {
+function trepzoid(param) {
     const [t, b, h] = checkParam(3,param);
     return (t+b)/2 * h;
 }
 
-function cylinder(...param) {
+function cylinder(param) {
     const [r, h] = checkParam(2, param);
-    return this.circle(r) * h
+    return this.circle([r]) * h
 }
 
+function recurCircle(start, end){
+    if(start === end ) {return Math.PI * end * end;}
+    return (Math.PI * start * start) + recurCircle(start+1, end);
+}
 
+function getArea(figure, ...param){
+    let answer;
+    if(figure !== "circle" ) {
+        answer = this[figure](param);
+    }
+    else if(figure === "circle") {
+        answer = param.length === 1 ? this[figure](param) : this["recurCircle"](...checkParam(2,param))
+    }
+    this.funcStack.push([figure, answer]) 
+    return answer
+}
+
+function printExecutionSequence(){
+    for (let el of this.funcStack) {
+        console.log(`호출 함수: ${el[0]} 결과: ${el[1]}`)
+    }
+}
 
 module.exports = {
         circle,
         rect,
         trepzoid,
         cylinder,
+        recurCircle,
+        getArea,
+        printExecutionSequence,
+        funcStack : []
     }
 
 
