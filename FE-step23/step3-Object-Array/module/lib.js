93459f27d4e31f5d7eeb6754325821bfdff18099
sel-dev <apricotsoul@gmail.com>
2019-04-12 16:00:11 +0900

    Add - isObject.js in Step3

---
+++ FE-step23/step3-Object-Array/module/lib.js
@@ -0,0 +1,14 @@
+const data = require("./data")
+
+// Returns if a value is an object
+function isObject (value) {
+    return value && typeof value === 'object' && value.constructor === Object;
+    }
+
+function makekNumArrFromObj (obj) {
+    let res = []
+    for (let key in obj) {
+        if()
+    }
+
+}
